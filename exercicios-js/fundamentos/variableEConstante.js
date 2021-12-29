f13e6c96a6fb3e311bf8c34d28f0216c3c3fb9d9
jhonmanauelg <jhonmanuelg6@outlook.com>
2021-12-29 17:53:09 -0300

    O basico de var let e const

---
+++ exercicios-js/fundamentos/variableEConstante.js
@@ -0,0 +1,16 @@
+//O basico de var let e const.
+var a = 3;  //palavra reservada VAR variable a asinar em 3.
+let b = 3; // recla geral só usar let para seu programa
+
+var a = 30;// var e declara a variable que despues se puede redefinir sim problema  a distinto let que da error
+
+b = 40;
+
+console.log(a, b); 
+
+a = 300;
+b = 400;
+console.log(a, b);
+
+const c = 50;
+console.log(c);
