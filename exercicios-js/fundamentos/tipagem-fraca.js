e375d7f0a8244c549b5c37483fc59b4eae695e17
jhonmanauelg <jhonmanuelg6@outlook.com>
2021-12-29 18:09:51 -0300

    Tipagem fraca mas não significa que no tenga tipo

---
+++ exercicios-js/fundamentos/tipagem-fraca.js
@@ -0,0 +1,18 @@
+//Tipagem fraca mas significa que no tenga tipo.
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.15;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nome genericos e siglas
+console.log("otros valores para dar espacio")
+
+let valor = '';
+let numero = 1;
+let pgp = false; // producto quimicos peligroso.... kkk
+
+
+//escolhar de bom nome é mas legal coloque comentario bem.
