c97a5f47cb41d81fc3d2ccd2465f71d056c5663f
jhonmanauelg <jhonmanuelg6@outlook.com>
2021-12-29 17:14:47 -0300

    comentarios

---
+++ comentarios.js
@@ -0,0 +1,15 @@
+//comentario de uma linha
+console.log('linha 1');
+
+/*
+comentario multiplas linha
+*/
+
+console.log('linha 2');
+
+/*
+*comentario de
+*muliplas linha
+*/
+
+console.log('linha 3')
