f19f8cd79bff8ace0f1b52f1554b2cb2ed93bd30
jhonmanauelg <jhonmanuelg6@outlook.com>
2021-12-29 14:43:22 -0300

    desafio em javasscript

--- inicio/desafio-area.js
+++ inicio/desafio-area.js
@@ -1,7 +1,13 @@
 //PI 3.14
 // PI * raio * raio
 const PI = 3.14;
-let raio = 10
+const raio = 10;
+
 
 let areaCirc = PI * raio * raio;
-console.log("o valor da area é " + areaCirc + "m2.")
+console.log("o valor da area é " + areaCirc + " m2.")
+//console.log(Math.PI);
+
+
+areaCirc = Math.PI * raio * raio;
+console.log("o valor da area é " + areaCirc + " m2.")
