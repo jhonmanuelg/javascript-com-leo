7879ceac8077994b2657c78a9f9a5bde45a01e91
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-01-02 05:45:36 -0300

    usando templete strings

---
+++ exercicios-js/fundamentos/templeteString.js
@@ -0,0 +1,22 @@
+//son cadenas literales de texto incrustadas en el código fuente que permiten su interpolación mediante expresiones 
+
+const nome = 'Rebeca'; 
+const concatenacao = 'Olá ' + nome + '!';
+
+const template = `
+ola
+${nome}!`;
+console.log(concatenacao, template);
+
+// expressoes
+console.log(`1 + 1 = ${1 + 1}`);
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`);
+
+/*
+Su sintaxis es sencilla y consta de dos aspectos fundamentales:
+
+Se utilizan las comillas invertidas (`) para delimitar las cadenas
+Para añadir marcadores de posición (o placeholders) utilizamos el signo de dolar ($) seguido de llaves
+*/
