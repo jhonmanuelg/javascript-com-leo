4c5cddb25bd772658cd125f5a6bb75f43eef4d33
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-02-08 13:49:41 -0300

    unsando let em LOOP

---
+++ exercicios-js/fundamentos/usandoLetEnLoop.js
@@ -0,0 +1,20 @@
+for (let i = 0; i < 4; i++) {
+    console.log(i)
+    // more statements
+ }
+
+ //console.log('i =', i)
+
+ var a = 5;
+var b = 10;
+
+if (a === 5) {
+  let a = 4; // O escopo é dentro do bloco if
+  var b = 1; // O escopo é dentro da função
+
+  console.log(a);  // 4
+  console.log(b);  // 1
+}
+
+console.log(a); // 5
+console.log(b); // 1
