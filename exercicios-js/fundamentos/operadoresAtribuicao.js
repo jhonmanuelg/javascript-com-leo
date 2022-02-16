831b15ace1e1654d1d478b354faba8072481f812
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-02-15 22:58:52 -0300

    Operadores atribuição

---
+++ exercicios-js/fundamentos/operadoresAtribuicao.js
@@ -0,0 +1,18 @@
+//Operadores atribuição
+
+const a = 7;
+let b = 4;
+b += a;  //a = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2; //b = b * 2
+console.log(b);
+
+b /= 5; // b = b / 5
+console.log(b)
+
+b %= 4; //b = b % 4
+console.log(b);
