af854959b2d97996d9c31b2cb886dfad91122f72
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-02-15 22:49:51 -0300

    A notação ponto.

---
+++ exercicios-js/fundamentos/notaçãoPonto.js
@@ -0,0 +1,24 @@
+//a notação ponto. 
+
+console.log(Math.ceil(6.1))
+
+const obj1 = {
+
+}
+
+obj1.nome = 'bola1'
+//obj1[nome] = 'bola2'
+console.log(obj1.nome)
+
+function obj(nome){
+    this.nome = nome
+    this.exec = function(){
+        console.log('exc...')
+    }
+}
+
+const obj2 = new obj('cadeira')
+const obj3 = new obj('mesa')
+console.log(obj2.nome)
+console.log(obj3.nome)
+obj3.exec();
