e6c727b9d20e6a5837da6132ef44055867d75d99
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-02-18 00:15:56 -0300

    [200~operadores Destructuring 4 usando arrey dentro de una parametro de una função

---
+++ exercicios-js/fundamentos/operadoresDestructuring4.js
@@ -0,0 +1,12 @@
+//operadores Destructuring 4 usando arrey dentro de una parametro de una função
+
+function rand([min = 0, max = 1000]){
+    if (min > max) [min, max] = [max, min]
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+console.log(rand([50, 40]));
+console.log(rand([992]));
+
+console.log(rand([, 10]))
