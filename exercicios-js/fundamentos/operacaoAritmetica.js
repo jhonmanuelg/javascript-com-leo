1fe632f470b295928b7df6c515e3d2f084dee4c9
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-02-20 23:08:02 -0300

    Operacao aritmetica

---
+++ exercicios-js/fundamentos/operacaoAritmetica.js
@@ -0,0 +1,10 @@
+// Operacao aritmetica
+const [a, b, c, d] = [3, 5, 1, 15]
+
+
+const soma = a + b + c + d;
+const subtracao = b - d;
+const multiplicacao = a * b;
+const divicao = d / a;
+const modulo = a % 2;
+console.groupCollapsed(soma, subtracao, multiplicacao, divicao, modulo)
