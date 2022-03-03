46a73f6f0f0d3cf1a8aaed59441c2d537f510803
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-03-02 21:42:21 -0300

    Project Clear

--- exercicios-js/fundamentos/operadorTernario.js
+++ exercicios-js/fundamentos/operadorTernario.js
@@ -3,3 +3,19 @@
 const resultado = nota => nota >= 7 ? 'aprovado' : 'reprobado';
 console.log(resultado(8.3))
 console.log(resultado(4.3))
+
+var firstCheck = false,
+    secondCheck = false,
+    access = firstCheck ? "Access denied" : secondCheck ? "Access denied" : "Access granted";
+
+console.log( access ); // logs "Access granted"
+
+var stop = false, age = 23;
+
+age > 18 ? (
+    alert("OK, you can go."),
+    location.assign("continue.html")
+) : (
+    stop = true,
+    alert("Sorry, you are much too young!")
+);
