524c02def58ad8d4fa91e0caa6f3b76fab8940e0
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-03-14 09:56:44 -0300

    Usado estruturas de controles IF, ELSE, IF.

---
+++ controle/ifElseIf.js
@@ -0,0 +1,24 @@
+// Usado estruturas de controles IF, ELSE, IF.
+
+
+Number.prototype.entre = function (inicio, fim){
+    return this >= inicio &&  this <= fim
+}
+
+const imprimirResultado = function (nota){
+    if (nota.entre(9, 10)) {
+        console.log('cuadro de honra' )
+    }else if(nota.entre(7, 8.99)){
+        console.log('aprovado')
+    } else if(nota.entre(4, 6.99)){
+        console.log('Reprovado')
+    }else if(nota.entre(0, 3.99)){
+        console.log('Nota invalida')
+    }
+    
+}
+imprimirResultado(10)
+imprimirResultado(8.5)
+imprimirResultado(7.5)
+imprimirResultado(4.5)
+imprimirResultado(1.6)
--- exercicios-js/fundamentos/operadoresDestructuring2.js
+++ exercicios-js/fundamentos/operadoresDestructuring2.js
@@ -1,6 +1,6 @@
 //operadores Destructuring parte 2
 
-const [a] = [10];
+const [a] = ['karla'];
 console.log(a)
 
 const [n1, , n3, n5, n6 = 0] = [10, 7, 9, 8 ];
