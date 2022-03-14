0a392659dd4262aa96454262c9ee297335d6ae78
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-03-14 11:38:56 -0300

    estrutura While

---
+++ controle/estruturaWhile.js
@@ -0,0 +1,14 @@
+//estrutura While é uma das Estruturas de Repetição que podemos utilizar para replicar um bloco de código e, consequentemente, eliminar duplicação. Se a condição declarada no while for verdadeira, o programa executará o código de dentro 
+
+function getInteiroAleatorioEntre(min, max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+let opcao = 0;
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opçao escolhida foi ${opcao}.`  )
+}
+
+console.log('Ate a Proxima!')
