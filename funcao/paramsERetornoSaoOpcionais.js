020fced36b46960150b96e77a0491e6b4a2d2f3b
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-03-17 09:37:21 -0300

    parametro E Retorno Sao Opcionais em uma funçao Js.

---
+++ funcao/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,17 @@
+// parametro E Retorno Sao Opcionais em uma funçao
+
+function area(largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log(`valor acima do permitido: ${area}m2.`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(4, 8))
+console.log(area(4, 4))
+console.log(area(2))
+console.log(area())
+console.log(area(4, 8, 7, 9))
+console.log(area(4, 8, 7, "hola"))
