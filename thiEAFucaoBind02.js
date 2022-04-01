6aa15120e85152d21a56c1137544232e44f161de
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-04-01 00:38:49 -0300

    this E A Fucao Bind js # 2

---
+++ thiEAFucaoBind02.js
@@ -0,0 +1,13 @@
+// this E A Fucao Bind js # 2
+
+function pessoa (){
+    this.idade = 0
+
+    const self = this
+    setInterval(function (){
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
+}
+
+new pessoa
