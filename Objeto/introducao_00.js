b3a3109c6d138f163121bb84e2467458eee73855
jhonmanauelg <jhonmanuelg6@outlook.com>
2022-04-21 02:44:29 -0300

    Intradução a OO

---
+++ Objeto/introducao_00.js
@@ -0,0 +1,27 @@
+/*
+codei não executavel!!!
+ = Intradução a OO
+ codigo sim resultado solo nuestra de OO
+ Programação Orientada á Objetos
+Podemos dizer que orientação á objetos basicamente é um modelo de programação baseado na criação
+ de Classes (receitas de bolo), nelas definimos como (atributos) e o que (métodos ou funções) 
+ nossos objetos irão ser ou fazer, como o assunto do post trata-se de aplicar esse modelo em 
+ JavaScript, não irei entrar muito em detalhes, para entender melhor o conceito, recomendo o artigo
+
+*/
+processamento(valor1, valor2, valor3);
+
+// OO 
+Object = {
+   valor1,
+   valor2,
+   valor3,
+   processamento (){
+       ///...
+   }
+}
+
+Object.processamento();
+
+//abitraçao 
+
